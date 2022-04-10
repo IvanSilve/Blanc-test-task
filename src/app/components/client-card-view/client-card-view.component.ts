aaba504d5b99c8af639054a6996765c28106282a
Ivan Serebryakov <ivan.serebryakov@enkod.io>
2022-04-10 13:06:58 +0300

    client-card-view rows keep it simple

--- src/app/components/client-card-view/client-card-view.component.ts
+++ src/app/components/client-card-view/client-card-view.component.ts
@@ -14,29 +14,7 @@ import { rowKeys } from './constants';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ClientCardViewComponent {
-  @Input() set data(data: any) {
-    this._data = data;
-    // можно было и без этого, мне больше нравится, когда template чистый
-    this.rows = Object.keys(data)
-      .filter((key) => {
-        return rowKeys.includes(key);
-      })
-      .map((key) => {
-        const fromClientCols = CLIENTS_COLS.find((col) => col.field === key);
-        return {
-          field: fromClientCols?.header,
-          value: data[key],
-          date: fromClientCols?.date,
-        };
-      });
-  }
-
-  private _data: any;
-  rows: any;
-
-  get data() {
-    return this._data;
-  }
+  @Input() data: any;
 
   get header() {
     return `Карточка клиента ${this.data.organizationName}`;
